2d39974c0953dc0e721d3b0dc562073bb2806f79
blindelectron <82350043+blindelectron@users.noreply.github.com>
2026-05-24 17:38:48 -0500

    tryes to fix mac os watchdog issues some more.

--- __init__.py
+++ __init__.py
@@ -2,4 +2,9 @@ import sys
 # we do this weerd workaround so that plugin code gets reloaded  when the refresh command is run, this should already happen but it seems broken at this time.
 if __name__ in sys.modules:
     del(sys.modules[__name__])
+# close old config if it exists
+from dwcom import config
+if config is not None:
+    config.close()
+    del(config)
 from dwcom import Trigger
